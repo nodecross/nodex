5b128bbbc522b643aa318c0ca0e7682587d958bf
Dongri Jin <dongrify@gmail.com>
2023-09-25 21:38:44 +0900

    Fix example code

--- examples/nodejs/src/version_update.ts
+++ examples/nodejs/src/version_update.ts
@@ -3,7 +3,7 @@ import { post } from './sock.js'
 (async () => {
   const json = await post('/internal/version/update', {
     message: {
-      "binary_url": "https://nilth.com/nodex-agent",
+      "binary_url": "https://example.com/nodex-agent-1.0.0",
       "path": "/tmp/nodex-agent",
     }
   })
