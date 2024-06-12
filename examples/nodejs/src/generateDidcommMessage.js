b7607a935ff7147b09a19707f3f0642035863879
ibuibu <ibuibu69@gmail.com>
2024-06-12 13:58:00 +0900

    modify examples

--- examples/nodejs/src/generateDidcommMessage.js
+++ examples/nodejs/src/generateDidcommMessage.js
@@ -1,23 +1,25 @@
 import { post } from "./lib/sock.js";
 
-(async () => {
-  const destination_did =
-    "did:nodex:test:DummyDummyDummyDummyDummyDummyDummyDummyDummyD";
-  const messageJson = {
-    message: {
-      string: "value",
-      number: 1,
-      boolean: true,
-      array: ["foo", "bar", "baz"],
-      map: { key: "value" },
-    },
-  };
-  const message = JSON.stringify(messageJson, null, 4);
+// PLEASE WRITE destination_did, messageJson, AND operation_tag.
+const destination_did =
+  "did:nodex:test:DummyDummyDummyDummyDummyDummyDummyDummyDummyD";
+const messageJson = {
+  message: {
+    string: "value",
+    number: 1,
+    boolean: true,
+    array: ["foo", "bar", "baz"],
+    map: { key: "value" },
+  },
+};
+const operation_tag = "test-operation-tag";
 
+(async () => {
+  const message = JSON.stringify(messageJson, null, 4);
   const response = await post("/create-didcomm-message", {
     destination_did,
     message,
-    operation_tag: "test-operation-tag",
+    operation_tag,
   });
 
   console.log('\nPlease paste below to "verifyDidcommMessage.js".\n');
