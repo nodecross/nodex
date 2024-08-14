75f1e977e8d6ad7e8b430b286f312c863d60daa2
daichitominaga <daichi.tominaga.1112@gmail.com>
2024-08-14 22:04:52 +0900

    chore: add install and remove script

--- omnibus/config/software/build-nodex-agent.rb
+++ omnibus/config/software/build-nodex-agent.rb
@@ -1,8 +1,6 @@
 name "build-nodex-agent"
 default_version "1.0.0"
 
-# dependency "zlib"
-
 build do
   nodex_dir = File.expand_path('..', Omnibus::Config.project_root)
   unless Dir.exist?("#{project_dir}/src")
@@ -19,15 +17,8 @@ build do
   copy "#{nodex_dir}/Cargo.toml", "#{project_dir}"
   copy "#{nodex_dir}/Cargo.lock", "#{project_dir}"
 
-  # env = with_standard_compiler_flags(with_embedded_path)
-  # command "cd #{project_dir} && cargo build --release", env: env
   command "cd #{project_dir} && cargo build --release"
-
-  # unless Dir.exist?("#{install_dir}/bin")
-  #   mkdir "#{install_dir}/bin"
-  # end
   copy "#{project_dir}/target/release/nodex-agent", "#{install_dir}/bin"
-  # command "ldd #{install_dir}/bin/nodex-agent | grep '=>' | awk '{print $3}' | xargs -I {} cp {} #{install_dir}/embedded/lib/"
 
   unless Dir.exist?("#{install_dir}/var/run")
     # prepare pid directory
--- omnibus/config/software/init-scripts.rb
+++ omnibus/config/software/init-scripts.rb
@@ -5,30 +5,16 @@ ohai = Ohai::System.new
 ohai.all_plugins
 
 build do
-  etc_dir = "/etc/nodex-agent"
-  if ohai['platform_family'] == 'debian' || ohai['platform_family'] == 'rhel'
-    mkdir "/etc/init"
-    if ohai['platform_family'] == 'debian'
-      # sysvinit support for debian only for now
-      mkdir "/etc/init.d"
-      # debian recommends using a different directory for systemd unit files
-      systemd_directory = "/lib/systemd/system"
-      erb source: "upstart_debian.conf.erb",
-          dest: "/etc/init/nodex-agent.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-
-      erb source: "sysvinit_debian.erb",
-          dest: "/etc/init.d/datadog-agent",
-          mode: 0755,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-
-      project.extra_package_file '/etc/init.d/datadog-agent'
-
-      erb source: "systemd.service.erb",
-          dest: "#{systemd_directory}/nodex-agent.service",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-    end
+  if ohai['platform_family'] == 'debian'
+    # sysvinit support for debian only for now
+    mkdir "/etc/init.d"
+    # debian recommends using a different directory for systemd unit files
+    etc_dir = "/etc/nodex-agent"
+    systemd_directory = "/lib/systemd/system"
+    # project.extra_package_file '/etc/init.d/nodex-agent'
+    erb source: "systemd.service.erb",
+        dest: "#{systemd_directory}/nodex-agent.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
   end
 end
