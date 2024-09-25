d414a24ee8f6fe4e20cac5d91fd090f5dbbbfe79
daichitominaga <daichi.tominaga.1112@gmail.com>
2024-09-25 21:34:03 +0900

    fix: build nodex-agent config

--- omnibus/config/software/build-nodex-agent.rb
+++ omnibus/config/software/build-nodex-agent.rb
@@ -3,17 +3,21 @@ default_version "1.0.0"
 
 build do
   nodex_dir = File.expand_path('..', Omnibus::Config.project_root)
-  unless Dir.exist?("#{project_dir}/src")
-    mkdir "#{project_dir}/src"
+  unless Dir.exist?("#{project_dir}/agent")
+    mkdir "#{project_dir}/agent"
   end
-  copy "#{nodex_dir}/src/*", "#{project_dir}/src/"
+  copy "#{nodex_dir}/agent/*", "#{project_dir}/agent/"
+
+  unless Dir.exist?("#{project_dir}/protocol")
+    mkdir "#{project_dir}/protocol"
+  end
+  copy "#{nodex_dir}/protocol/*", "#{project_dir}/protocol/"
 
   unless Dir.exist?("#{project_dir}/e2e")
     mkdir "#{project_dir}/e2e"
   end
   copy "#{nodex_dir}/e2e/*", "#{project_dir}/e2e/"
 
-  copy "#{nodex_dir}/build.rs", "#{project_dir}"
   copy "#{nodex_dir}/Cargo.toml", "#{project_dir}"
   copy "#{nodex_dir}/Cargo.lock", "#{project_dir}"
 
