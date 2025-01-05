72732f72d5abbb430eb6cd1c1cb2bb977f8f7d60
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2025-01-05 23:12:03 +0900

    fix: build.rs

--- controller/build.rs
+++ controller/build.rs
@@ -1,3 +1,6 @@
-fn main() -> shadow_rs::SdResult<()> {
-    shadow_rs::new()
+fn main() {
+    shadow_rs::ShadowBuilder::builder()
+        .build_pattern(shadow_rs::BuildPattern::RealTime)
+        .build()
+        .unwrap();
 }
