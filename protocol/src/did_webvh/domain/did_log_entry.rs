3c27b00497be95bf4392a5d1ffc91d304f15ec83
3kz2501 <ghub@nlnll.xyz>
2025-01-27 23:33:12 +0900

    rename did log entry domain, DID to Did

--- protocol/src/did_webvh/domain/did_log_entry.rs
+++ protocol/src/did_webvh/domain/did_log_entry.rs
@@ -12,7 +12,7 @@ const WEBVH_DID_CRYPTO_SUITE: &str = "eddsa-jcs-2022";
 const WEBVH_DID_SCID_PLACEHOLDER: &str = "{SCID}";
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum DIDLogEntryError {
+pub enum DidLogEntryError {
     InvalidVersionId,
     InvalidVersionTime,
     InvalidParameters,
@@ -25,7 +25,7 @@ pub enum DIDLogEntryError {
 
 #[derive(Serialize, Deserialize, Debug, Clone, Validate, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct DIDLogEntry {
+pub struct DidLogEntry {
     #[validate(custom(function = "verify_version_id"))]
     pub version_id: String,
     #[validate(custom(function = "verify_version_time"))]
@@ -182,11 +182,11 @@ fn verify_proof_created(created: &str) -> Result<(), ValidationError> {
     Ok(())
 }
 
-impl DIDLogEntry {
+impl DidLogEntry {
     // Create a new DIDLogEntry, with scid placeholder
-    pub fn new(uri: &str) -> Result<Self, DIDLogEntryError> {
+    pub fn new(uri: &str) -> Result<Self, DidLogEntryError> {
         let did = DidWebvh::new(WEBVH_DID_SCID_PLACEHOLDER, uri)
-            .map_err(|_| DIDLogEntryError::InvalidFormat)?;
+            .map_err(|_| DidLogEntryError::InvalidFormat)?;
         let state = DidDocument::new(did.get_did().clone());
         let version_id = WEBVH_DID_SCID_PLACEHOLDER.to_string();
         let version_time = chrono::Utc::now().to_rfc3339();
@@ -210,7 +210,7 @@ impl DIDLogEntry {
     }
 
     // create a new DIDLogEntry from current entry.
-    pub fn generate_next_log_entry(&self) -> Result<Self, DIDLogEntryError> {
+    pub fn generate_next_log_entry(&self) -> Result<Self, DidLogEntryError> {
         let (_, next_version_id) = self.parse_verion_id()?;
         let version_time = chrono::Utc::now().to_rfc3339();
         Ok(Self {
@@ -222,25 +222,25 @@ impl DIDLogEntry {
         })
     }
 
-    pub fn parse_verion_id(&self) -> Result<(u32, String), DIDLogEntryError> {
+    pub fn parse_verion_id(&self) -> Result<(u32, String), DidLogEntryError> {
         let parts: Vec<&str> = self.version_id.split('-').collect();
         if parts.len() != 2 {
-            return Err(DIDLogEntryError::InvalidVersionId);
+            return Err(DidLogEntryError::InvalidVersionId);
         }
         let version_number = parts[0].parse::<u32>();
         if version_number.is_err() {
-            return Err(DIDLogEntryError::InvalidVersionId);
+            return Err(DidLogEntryError::InvalidVersionId);
         }
         Ok((version_number.unwrap(), parts[1].to_string()))
     }
 
-    pub fn replace_to_scid_placeholder(&self) -> Result<DIDLogEntry, DIDLogEntryError> {
+    pub fn replace_to_scid_placeholder(&self) -> Result<DidLogEntry, DidLogEntryError> {
         let scid_placeholder = "{SCID}";
         let mut entry = self.clone();
         entry.parameters.scid = Some(scid_placeholder.to_string());
         entry.version_id = scid_placeholder.to_string();
         let did = DidWebvh::try_from(entry.state.id.clone())
-            .map_err(|_| DIDLogEntryError::InvalidState)?
+            .map_err(|_| DidLogEntryError::InvalidState)?
             .replace_scid(scid_placeholder);
         entry.state.id = did.into();
         Ok(entry)
@@ -266,26 +266,26 @@ impl DIDLogEntry {
     // calculate the entry hash
     // The first entry, LogEntry with SCID placeholder can use this function to compute the SCID.
     // Otherwise, just be calculated as an EntryHash
-    pub fn calc_entry_hash(&self) -> Result<String, DIDLogEntryError> {
+    pub fn calc_entry_hash(&self) -> Result<String, DidLogEntryError> {
         let mut entry = self.clone();
         if entry.proof.is_some() {
             entry.proof = None;
         }
         let jcs = serde_json_canonicalizer::to_string(&entry)
-            .map_err(|_| DIDLogEntryError::InvalidFormat)?;
+            .map_err(|_| DidLogEntryError::InvalidFormat)?;
         generate_multihash_with_base58encode(jcs.as_bytes())
-            .map_err(|_| DIDLogEntryError::FaildMultihash)
+            .map_err(|_| DidLogEntryError::FaildMultihash)
     }
 
     // calculate the next key hashes by the Update Keys from the previous entry.
-    pub fn calc_next_key_hash(&self, keys: &Vec<String>) -> Result<Self, DIDLogEntryError> {
+    pub fn calc_next_key_hash(&self, keys: &Vec<String>) -> Result<Self, DidLogEntryError> {
         let generate_hashed_keys = |keys: &Vec<String>| {
             keys.iter()
                 .map(|key| {
                     generate_multihash_with_base58encode(key.as_bytes())
-                        .map_err(|_| DIDLogEntryError::FaildMultihash)
+                        .map_err(|_| DidLogEntryError::FaildMultihash)
                 })
-                .collect::<Result<Vec<String>, DIDLogEntryError>>()
+                .collect::<Result<Vec<String>, DidLogEntryError>>()
         };
         let mut entry = self.clone();
         entry.parameters.next_key_hashes = Some(generate_hashed_keys(keys)?);
@@ -304,7 +304,7 @@ mod tests {
         {"versionId": "3-QmVUpHdsP2LtPbuCVAmSApSDNfn9AeY3GVWuC9FXWByA3C", "versionTime": "2024-10-01T22:13:49Z", "parameters": {}, "state": {"@context": ["https://www.w3.org/ns/did/v1", "https://w3id.org/security/multikey/v1", "https://identity.foundation/.well-known/did-configuration/v1", "https://identity.foundation/linked-vp/contexts/v1"], "id": "did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example", "authentication": ["did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example#z6MkijyunEqPi7hzgJirb4tQLjztCPbJeeZvXEySuzbY6MLv"], "assertionMethod": ["did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example#z6MkijyunEqPi7hzgJirb4tQLjztCPbJeeZvXEySuzbY6MLv"], "verificationMethod": [{"id": "did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example#z6MkijyunEqPi7hzgJirb4tQLjztCPbJeeZvXEySuzbY6MLv", "controller": "did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example", "type": "Multikey", "publicKeyMultibase": "z6MkijyunEqPi7hzgJirb4tQLjztCPbJeeZvXEySuzbY6MLv"}], "service": [{"id": "did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example#domain", "type": "LinkedDomains", "serviceEndpoint": "https://domain.example"}, {"id": "did:webvh:QmaJp6pmb6RUk4oaDyWQcjeqYbvxsc3kvmHWPpz7B5JwDU:domain.example#whois", "type": "LinkedVerifiablePresentation", "serviceEndpoint": "https://domain.example/.well-known/whois.vc"}]}, "proof": [{"type": "DataIntegrityProof", "cryptosuite": "eddsa-jcs-2022", "verificationMethod": "did:key:z6MkoSFjacZb7R82htx8n1AkpgLQWR7CA6rigsc2VH9acLuF#z6MkoSFjacZb7R82htx8n1AkpgLQWR7CA6rigsc2VH9acLuF", "created": "2024-10-01T22:13:49Z", "proofPurpose": "authentication", "proofValue": "z32PcoCy9cRWBTUX8M9k5zNGunMnnn36B7yjwSnHJED7UfRC1EYJEDWiWP5yTdxy8QNKZRCitSDk4wzBtQM4nxNUj"}]}"#;
     #[test]
     fn test_did_log_entry_deserialization() {
-        let entry: DIDLogEntry = serde_json::from_str(JSON).unwrap();
+        let entry: DidLogEntry = serde_json::from_str(JSON).unwrap();
         assert_eq!(
             entry.version_id,
             "1-QmRD52wqs942kZ2gs7UU9QmaopvqnMziqB4qgFDYsapCT9"
@@ -315,7 +315,7 @@ mod tests {
 
     #[test]
     fn test_did_log_entries_deserialization() {
-        let entries: Vec<DIDLogEntry> = JSONL
+        let entries: Vec<DidLogEntry> = JSONL
             .lines()
             .map(|line| match serde_json::from_str(line) {
                 Ok(entry) => entry,
@@ -343,13 +343,13 @@ mod tests {
 
     #[test]
     fn test_log_entry_properties() {
-        let entry: DIDLogEntry = serde_json::from_str(JSON).unwrap();
+        let entry: DidLogEntry = serde_json::from_str(JSON).unwrap();
 
         let (version_number, hash) = entry.parse_verion_id().unwrap();
         assert_eq!(version_number, 1);
         assert_eq!(hash, "QmRD52wqs942kZ2gs7UU9QmaopvqnMziqB4qgFDYsapCT9");
 
-        let entry: DIDLogEntry = serde_json::from_str(JSON).unwrap();
+        let entry: DidLogEntry = serde_json::from_str(JSON).unwrap();
         let new_entry = entry.replace_to_scid_placeholder().unwrap();
         assert_eq!(new_entry.parameters.scid.unwrap(), "{SCID}");
         assert_eq!(new_entry.version_id, "{SCID}");
@@ -374,7 +374,7 @@ mod tests {
     "id": "did:webvh:{SCID}:example.com:eve"
   }
 }"#;
-        let mut entry: DIDLogEntry = serde_json::from_str(JSON_LOG).unwrap();
+        let mut entry: DidLogEntry = serde_json::from_str(JSON_LOG).unwrap();
         println!("ENTRY: {:?}\n", entry);
         let scid = entry.calc_entry_hash().unwrap();
         assert_eq!(scid, "QmbUzhqS4Fx6ueq6gopKQBNe2Dyj4dddCTyPuN4pncYxYG");
