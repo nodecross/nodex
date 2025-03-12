5ec372540344cea7fe6be6481b56e5bcc65c90e9
3kz2501 <ghub@nlnll.xyz>
2025-03-12 22:01:51 +0900

    refactor: update nodex find identifier handler to use Uri

--- agent/src/controllers/public/nodex_find_identifier.rs
+++ agent/src/controllers/public/nodex_find_identifier.rs
@@ -1,14 +1,21 @@
 use crate::controllers::errors::AgentErrorCode;
 use crate::nodex::utils::webvh_client::DidWebvhDataStoreImpl;
 use crate::server_config;
-use axum::extract::{Json, Path};
+use axum::extract::Json;
+use axum::http::Uri;
 use protocol::did_webvh::domain::did::Did;
 use protocol::did_webvh::domain::did_document::DidDocument;
 use protocol::did_webvh::service::resolver::resolver_service::DidWebvhResolverService;
 use protocol::did_webvh::service::service_impl::DidWebvhServiceImpl;
 use std::str::FromStr;
 
-pub async fn handler(Path(did): Path<String>) -> Result<Json<Option<DidDocument>>, AgentErrorCode> {
+pub async fn handler(uri: Uri) -> Result<Json<Option<DidDocument>>, AgentErrorCode> {
+    let raw_path = uri.path();
+    let did = if raw_path.starts_with("/identifiers/") {
+        &raw_path[13..]
+    } else {
+        return Err(AgentErrorCode::FindIdentifierInternal)?;
+    };
     let server_config = server_config();
     let baseurl = url::Url::parse(&server_config.did_http_endpoint())
         .map_err(|_| AgentErrorCode::FindIdentifierInternal)?;
