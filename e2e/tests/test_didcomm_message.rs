c3b12ba8bc2b34a7d519f63138bed4f6f4405820
3kz2501 <ghub@nlnll.xyz>
2025-04-03 20:18:20 +0900

    ci(e2e): add mock server setup for e2e tests on mac and windows

--- e2e/tests/test_didcomm_message.rs
+++ e2e/tests/test_didcomm_message.rs
@@ -32,6 +32,7 @@ async fn create_didcomm_message_scenario() -> anyhow::Result<String> {
     let body: String = response_to_string(response).await?;
     // parse check
     let parsed = serde_json::from_str::<serde_json::Value>(&body)?;
+    dbg!(&parsed);
 
     Ok(body)
 }
